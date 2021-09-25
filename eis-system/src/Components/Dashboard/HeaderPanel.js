707c9552c862fe888d3020620d483a713cfc0518
Josiah Autor <autorjosiah@gmail.com>
2021-09-25 14:49:25 +0800

    add icons for HeaderPanel

--- eis-system/src/Components/Dashboard/HeaderPanel.js
+++ eis-system/src/Components/Dashboard/HeaderPanel.js
@@ -1,8 +1,11 @@
-import tempLogo from "./tempLogo.png";
 
-let salesRevenue = "₱ " + 1432.04;
-let profit = "₱ " + 392.94;
-let soldUnits = 4206;
+import profit from "./Assets/profit.png";
+import salesRevenue from "./Assets/salesRevenue.png";
+import soldUnits from "./Assets/soldUnits.png";
+
+let sales_Revenue = "₱ " + 1432.04;
+let profits = "₱ " + 392.94;
+let sold_Units = 4206;
 let salesTarget = "₱ " + 1300;
 let salesTargetPercent = 100 + "%";
 
@@ -12,30 +15,30 @@ const HeaderPanel = () => {
       <div className="alignMiddle childPanel1">
         <div>
           <div className="alignMiddle inlineBlock childLogo">
-            <img src={tempLogo} width="100" height="100"></img>
+            <img src={salesRevenue} width="100" height="100"></img>
           </div>
           <div className="alignMiddle inlineBlock childLogo2">
-            <h1 className="noMargin">{salesRevenue}</h1>
+            <h1 className="noMargin">{sales_Revenue}</h1>
             <span className="panelLabel">Sales Revenue</span>
           </div>
         </div>
         <div>
           <div className="alignMiddle inlineBlock childLogo">
-            <img src={tempLogo} width="100" height="100"></img>
+            <img src={profit} width="100" height="100"></img>
           </div>
           <div className="alignMiddle inlineBlock childLogo2">
-            <h1 className="noMargin">{profit}</h1>
+            <h1 className="noMargin">{profits}</h1>
             <span>Profit</span>
           </div>
         </div>
       </div>
       <div className="alignMiddle childPanel2">
         <div>
-          <h1 className="noMargin">{soldUnits}</h1>
+          <h1 className="noMargin">{sold_Units}</h1>
           <span>Sold Units</span>
         </div>
         <div>
-          <img src={tempLogo} width="100" height="100"></img>
+          <img src={soldUnits} width="100" height="100"></img>
         </div>
       </div>
       <div className="alignMiddle childPanel3 ">
