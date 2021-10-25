c2b4abd9961f8b08914644ead78006575c4651b5
Haniel023 <54757754+Haniel023@users.noreply.github.com>
2021-10-25 23:00:36 +0800

    Add Developer Page

--- frontend/src/util/routing/routes.js
+++ frontend/src/util/routing/routes.js
@@ -6,7 +6,7 @@ import LoginForm from "../../Components/Login/LoginForm";
 import RegisterForm from "../../Components/Register/RegisterForm";
 import Dashboard from "../../Components/Dashboard/Dashboard";
 import Support from "../../Components/Support/Support";
-
+import DeveloperPage from "../../Components/DeveloperPage/DeveloperPage";
 //All the routes file
 const Routes = () => {
   return (
@@ -16,6 +16,7 @@ const Routes = () => {
         <Route exact path="/Register" component={RegisterForm} />
         <PrivateRoute exact path="/Dashboard" component={Dashboard} />
         <Route exact path="/Support" component={Support} />
+        <Route exact path="/DevPage" component={DeveloperPage} />
       </Switch>
     </div>
   );
