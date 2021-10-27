8edd76594e9e5533536aa1e774401e50092e804e
jervey <pesiganjervey29@gmail.com>
2021-10-27 14:14:09 +0800

    add dev page

--- frontend/src/Components/DeveloperPage/DeveloperPage.js
+++ frontend/src/Components/DeveloperPage/DeveloperPage.js
@@ -2,8 +2,33 @@ import React from 'react'
 
 const DeveloperPage = () => {
     return (
-        <div>
+        <div className="Container1">
+        <h1 id="devPage">Developer's pages</h1>
+        <div className="devFullStack">
             
+            <h2>Jether Deleon</h2>
+            <h5 className= "devPage1">Full Stack Developer</h5>
+        </div>
+        <div className="devFrontEnd">
+            <h2 >Josiah Author</h2>
+            <h5>Frontend Developer</h5>
+        </div>
+        <div className="devFrontEnd1">
+            <h2>Jan Nico Louise Gabo</h2>
+            <h5>Frontend Developer</h5>
+        </div>
+        <div className="devFrontEnd2">
+            <h2>Vernon Tamba</h2>
+            <h5>Frontend Developer</h5>
+        </div>
+        <div className="devFrontEnd3">
+            <h2>Jared Ibanez</h2>
+            <h5>Frontend Developer</h5>
+        </div>
+        <div className="devFrontEnd4">
+            <h2>Jervey Pesigan</h2>
+            <h5>Frontend Developer</h5>
+        </div>
         </div>
     )
 }
