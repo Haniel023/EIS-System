9845eb0bd14907a737ddb666289d76fb6a69384f
Nico <gabo.janicolo@gmail.com>
2021-11-14 13:34:32 +0800

    added icons to nav bar

--- frontend/src/Components/Dashboard/Sidebar.js
+++ frontend/src/Components/Dashboard/Sidebar.js
@@ -1,21 +1,29 @@
 import logo from "./Assets/EISLogo.png";
+import icoDashboard from "./Assets/icoDashboard.png";
+import icoSupport from "./Assets/icoSupport.png";
+import icoDevelopers from "./Assets/icoDevelopers.png";
+import icoLogout from "./Assets/icoLogout.png";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <div className="sidelogo">
-                <img src={logo} width="40" height="40"></img>
+            <div>
+                <img src={logo} className="sideLogo"></img>
             </div>
             <button className="sidebarButton">
+                <img src={icoDashboard} className="buttonLogo"></img>
                 Dashboard
             </button>
             <button className="sidebarButton">
+                <img src={icoSupport} className="buttonLogo"></img>
                 Support
             </button>
             <button className="sidebarButton">
+            <img src={icoDevelopers} className="buttonLogo"></img>
                 Developers
             </button>
             <button className="sidebarButton logoutButton">
+            <img src={icoLogout} className="buttonLogo"></img>
                 Logout
             </button>
         </div>
