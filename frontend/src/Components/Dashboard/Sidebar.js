58dfd6ec98b01acb781f48b1d59cb02e52323cc2
Haniel023 <54757754+Haniel023@users.noreply.github.com>
2021-11-14 19:45:00 +0800

    polish frontend

--- frontend/src/Components/Dashboard/Sidebar.js
+++ frontend/src/Components/Dashboard/Sidebar.js
@@ -3,22 +3,25 @@ import icoDashboard from "./Assets/icoDashboard.png";
 import icoSupport from "./Assets/icoSupport.png";
 import icoDevelopers from "./Assets/icoDevelopers.png";
 import icoLogout from "./Assets/icoLogout.png";
+import { useHistory } from "react-router-dom";
 
 const Sidebar = () => {
+    const History = useHistory();
+    
     return (
         <div className="sidebar">
             <div>
                 <img src={logo} className="sideLogo"></img>
             </div>
-            <button className="sidebarButton">
+            <button className="sidebarButton" onClick={() => { History.push("/Dashboard") }}>
                 <img src={icoDashboard} className="buttonLogo"></img>
                 Dashboard
             </button>
-            <button className="sidebarButton">
+            <button className="sidebarButton" onClick={() => { History.push("/Support") }}>
                 <img src={icoSupport} className="buttonLogo"></img>
                 Support
             </button>
-            <button className="sidebarButton">
+            <button className="sidebarButton" onClick={() => { History.push("/DevPage") }}>
             <img src={icoDevelopers} className="buttonLogo"></img>
                 Developers
             </button>
